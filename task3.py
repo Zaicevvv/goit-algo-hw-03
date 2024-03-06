ce43c9c9d153fbf898768fe7770e15223f03f643
Vladimir <vladimir.zaitsev@jointoit.com>
2024-03-06 17:51:36 +0200

    third task

---
+++ task3.py
@@ -0,0 +1,25 @@
+from re import sub
+
+def normalize_phone(phone_number):
+    pattern = r"\D+"
+    replacement = ""
+    phone_number = sub(pattern, replacement, phone_number)
+    phone_number = '+' + phone_number if phone_number.startswith('38') \
+        else '+38' + phone_number
+    return phone_number
+
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22   ",
+    "38050 111 22 11",
+]
+
+normalized_numbers = [normalize_phone(num) for num in raw_numbers]
+
+print(normalized_numbers)
