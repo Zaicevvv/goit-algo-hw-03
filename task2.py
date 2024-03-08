814c38284e25289030f3dc9cd10fef478970ee70
Vladimir <vladimir.zaitsev@jointoit.com>
2024-03-08 21:34:56 +0200

    second task fix

--- task2.py
+++ task2.py
@@ -2,19 +2,20 @@ from random import randint
 
 def get_numbers_ticket(min, max, quantity):
     random_numbers = set()
-    print(min >= quantity >= max)
+    
     if type(min) != int or type(max) != int or type(quantity) != int:
         print('All values must be integer')
     elif min < 1 or max > 1000:
         print(f"Values of min:{min} and max:{max} does not match format " \
               "min >= 1 and max <= 1000")
-    elif not 0 < quantity < max:
+    elif not 0 < quantity <= max or quantity > (max - min + 1):
         print(f"Quantity:{quantity} should be in range of " \
-              f"min:1 and max:{max - 1}")
+              f"1 and max:{max} and shouldn't be grater " \
+              f"than max-min+1:{max - min + 1}")
     else:
         while len(random_numbers) < quantity:
             random_numbers.add(randint(min, max))
 
     return sorted(list(random_numbers))
         
-print(get_numbers_ticket(1, 49, 49))
+print(get_numbers_ticket(1, 2, 2))
